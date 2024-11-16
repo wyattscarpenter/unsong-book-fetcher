ab7cd71ddd7aacdb0fb1f6427314495d0b5f16ad
wyattscarpenter <wyattscarpenter@gmail.com>
2024-11-15 23:23:31 -0800

    update comments in the bat script

--- create_unsong_book.bat
+++ create_unsong_book.bat
@@ -1,5 +1,6 @@
-#!/bin/bash
-# this is both a valid batch script and a valid bash script
+#!/bin/sh
+# this is both a valid Windows batch script and a unix shell script
+# remake this dir just in case:
 mkdir cache
 pip3 install -r requirements.txt && python3 get_unsong.py || python get_unsong.py
 ebook-convert Unsong.html Unsong.epub --level1-toc="//h:h1" --level2-toc="//h:h2" --no-default-epub-cover --authors "Scott Alexander" --language en
