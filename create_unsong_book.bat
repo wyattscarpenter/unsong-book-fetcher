df0409dff10db63c1ffa877e62c6eb6959bef57f
wyattscarpenter <wyattscarpenter@gmail.com>
2024-11-22 00:47:52 -0800

    Simplfy bat script a bit to try to blindly fix https://github.com/wyattscarpenter/unsong-book-fetcher/issues/20

--- create_unsong_book.bat
+++ create_unsong_book.bat
@@ -1,3 +1,3 @@
-# this is both a valid Windows batch script and a unix shell script
-pip3 install -r requirements.txt -qq && python3 get_unsong.py || python get_unsong.py
+pip3 install -r requirements.txt -qq
+python3 get_unsong.py || python get_unsong.py
 ebook-convert ebooks/Unsong.html ebooks/Unsong.epub --level1-toc="//h:h1" --level2-toc="//h:h2" --no-default-epub-cover --authors "Scott Alexander" --language en
